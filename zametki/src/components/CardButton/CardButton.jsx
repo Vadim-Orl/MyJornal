df56dfa47f5b7f90d35c3db115443cc84c68eaae
vad-orl <vad-orl90@mail.ru>
2024-02-11 22:55:51 +0300

    чистка кода

--- zametki/src/components/CardButton/CardButton.jsx
+++ zametki/src/components/CardButton/CardButton.jsx
@@ -2,7 +2,6 @@ import style from './CardButton.module.css';
 
 function CardButton({children, className, ...props}) {
 	const cl = `${style['card-button']} ${className ? className : ''}`;
-	// console.log(className);
 	return (
 		<button className={cl} {...props}>{children}</button>
 	);
